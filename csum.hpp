144dae7c070c91c9ef6c39e626b8b1298eb2f5bd
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-08-12 16:14:23 +0900

    csum

---
+++ csum.hpp
@@ -0,0 +1,34 @@
+
+// algocpp/numeric/csum.hpp
+//
+// this file is part of algocpp and is copyrighted by algocpp.
+// if used, it must comply with the mit license.
+
+#ifndef ALGOCPP_NUMERIC_CSUM
+#define ALGOCPP_NUMERIC_CSUM
+
+#include <string>
+#include <algocpp/type/ctoi.hpp>
+
+namespace algocpp
+{
+	namespace numeric
+	{
+		namespace base
+		{
+			inline unsigned long long csum(std::string s)
+			{
+				unsigned long long result = 0;
+				for (unsigned long long i = 0; i < s.size(); i++)
+				{
+					if (s[i] >= '0' && s[i] <= '9')
+						result += algocpp::type::ctoi(s[i]);
+				}
+
+				return result;
+			}
+		}
+	}
+}
+
+#endif // ALGOCPP_NUMERIC_CSUM
