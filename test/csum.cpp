025ace3ba51e81c9a7af6951762176fdbbcda529
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-09-22 19:06:21 +0900

    c

--- test/csum.cpp
+++ test/csum.cpp
@@ -1,6 +1,7 @@
 
 #include "../csum.hpp"
 #include <gtest/gtest.h>
+#include <string>
 
 using namespace algocpp::numeric;
 
@@ -8,4 +9,14 @@ TEST(numeric_csum, nomal)
 {
 	int a = 334;
 	EXPECT_EQ(csum(a), 10);
+}
+
+TEST(numeric_csum, string)
+{
+	std::string tmp = "1";
+	for (int i = 1; i <= 10000; ++i)
+	{
+		tmp += "1";
+		EXPECT_EQ(csum(tmp), i + 1);
+	}
 }
